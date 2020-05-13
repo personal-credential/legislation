796a94dcb1480387c4d6a11ebb6ff0a970f7a4fe
mukhulaazam <mukhulaazam@gmail.com>
2020-05-13 23:03:31 +0600

    13.event fetching from dom and others and next is v-model

--- src/components/first/Intro.vue
+++ src/components/first/Intro.vue
@@ -2,6 +2,17 @@
     <div class="intro">
         <h3>{{ showTitle() }}</h3>
         <span>For more details <a v-bind:href="link" target="_blank">Click here</a></span>
+        <hr>
+        <p v-html="sanitizer"></p>
+        <hr>
+        <button v-on:click="increase">Click for count number</button>
+        <h4>{{ counter }}</h4>
+        <button v-on:click="increaseAnother(2, $event)">Click for count number</button>
+        <h4>{{ counter }}</h4>
+        <button v-on:click="decrease">Click for count number</button>
+        <h4>{{ counter }}</h4>
+        <hr>
+        <p v-on:mousemove="updateCoordinates">Coordinates : {{ x }} / {{ y }} -- <span @mousemove="death">Do not hover here man </span> </p>
     </div>
 </template>
 
@@ -10,13 +21,35 @@ export default {
     data() {
         return {
             title: 'Dr. Mamun Chowdhury',
-            link: 'https://www.google.com/'
+            link: 'https://www.google.com/',
+            sanitizer : '<h5>Hello! there who are you? and where you came from? <span> there is nothing man</span></h5>',
+            counter: 0,
+            x:0,
+            y:0
         }
     },
     methods: {
         showTitle: function () {
             return this.title;
         },
+        increase: function () {
+            this.counter++;
+        },
+        increaseAnother: function (step, event) {
+            this.counter +=step;
+        },
+        decrease: function () {
+            this.counter--;
+        },
+        updateCoordinates: function (event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        },
+        death: function (event){
+            event.stopPropagation();
+            
+        }
+
     },
 }
 </script>
