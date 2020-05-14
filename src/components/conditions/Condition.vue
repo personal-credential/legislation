9591c63ce597de55c0cf5facfa88d6fb3f8cddff
mukhulaazam <mukhulaazam@gmail.com>
2020-05-14 23:36:35 +0600

    v-if staeted and next is

--- src/components/conditions/Condition.vue
+++ src/components/conditions/Condition.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
-        
+        <h2 style="text-align: centern">Conditions & Rendaring</h2>
+        <p v-if="show">You can see me</p>
+        <p>Do you can see me?</p>
+        <button @click="show = !show">Click</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            show: true,
+        }
+    },
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Intro from '@/components/first/Intro'
+import Condition from '@/components/conditions/Condition'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/intro',
       name: 'Intro',
       component: Intro
-    }
+    },
+    {
+      path: '/condition',
+      name: 'Condition',
+      component: Condition
+    },
   ]
 })
