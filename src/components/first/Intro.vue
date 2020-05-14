e4778be52bbfef5a1726cfecb23bec2cddff1f57
mukhulaazam <mukhulaazam@gmail.com>
2020-05-14 14:36:53 +0600

    Computed finished and next is

--- src/components/first/Intro.vue
+++ src/components/first/Intro.vue
@@ -13,6 +13,18 @@
         <h4>{{ counter }}</h4>
         <hr>
         <p v-on:mousemove="updateCoordinates">Coordinates : {{ x }} / {{ y }} -- <span @mousemove="death">Do not hover here man </span> </p>
+        <hr>
+        <input type="text" name="" id="" @keyup.enter.space="alertMe">
+        <hr>
+        <input type="text" v-model="name">
+        <p>{{ name }}</p>
+        <hr>
+        <hr>
+        <h3>Computed and watch</h3>
+        <button @click="counters++">Click Here</button>
+        <button @click="counters--">Click Here</button>
+        <span>{{ counters }}</span>
+        <p>{{ result() }}  || {{ output }}</p>
     </div>
 </template>
 
@@ -20,12 +32,21 @@
 export default {
     data() {
         return {
+            name: 'Dr. Mamun',
             title: 'Dr. Mamun Chowdhury',
             link: 'https://www.google.com/',
             sanitizer : '<h5>Hello! there who are you? and where you came from? <span> there is nothing man</span></h5>',
             counter: 0,
             x:0,
-            y:0
+            y:0,
+            counters:0,
+            
+        }
+    },
+    computed: {
+        output: function () {
+            console.log('computed')
+            return this.counters > 5 ? 'Greater 5' : 'Smaller 5'
         }
     },
     methods: {
@@ -48,6 +69,13 @@ export default {
         death: function (event){
             event.stopPropagation();
             
+        },
+        alertMe: function () {
+            alert('Stop Fucking!');
+        },
+        result:function () {
+            console.log('methods')
+            return this.counters > 5 ? 'Greater 5' : 'Smaller 5'
         }
 
     },
