cd5441412b13c8d14d14a55dff700132c51f5a09
mukhulaazam <mukhulaazam@gmail.com>
2020-05-14 22:41:58 +0600

    Interact with dom finished and next is conditions

--- src/components/first/Intro.vue
+++ src/components/first/Intro.vue
@@ -49,6 +49,14 @@ export default {
             return this.counters > 5 ? 'Greater 5' : 'Smaller 5'
         }
     },
+    watch: {
+        counter: function (value){
+            var vm = this;
+            setTimeout(function() {
+                vm.counter = 0;
+            },2000);
+        },
+    },
     methods: {
         showTitle: function () {
             return this.title;
